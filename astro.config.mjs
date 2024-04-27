789b06874a61f90d84026e6984ef44b676a2e9f6
Work <work@Holliss-MacBook-Pro.local>
2024-04-27 11:43:04 -0500

    fix closing Typography tag

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
-
 import preact from "@astrojs/preact";
 
 // https://astro.build/config
